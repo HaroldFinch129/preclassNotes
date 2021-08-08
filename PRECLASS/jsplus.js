b3f2418a86bcb8605a7df29f82e87ec35bd25f82
HaroldFinch129 <81425579+HaroldFinch129@users.noreply.github.com>
2021-08-08 14:51:37 +0300

    Add files via upload

---
+++ PRECLASS/jsplus.js
@@ -0,0 +1,10 @@
+let arr = [1, 2, 3, 4, 5];
+for (a of arr) {;
+    console.log(a);
+};
+
+let obj = { name: "Edward", lastname: "King", age: "47" };
+
+for (key in obj) {
+    console.log(key);
+};
