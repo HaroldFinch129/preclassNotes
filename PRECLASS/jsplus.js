77dcd881b8fa1c66128382805eba417f5b0d12cc
HaroldFinch129 <81425579+HaroldFinch129@users.noreply.github.com>
2021-08-08 15:00:34 +0300

    Add files via upload

--- PRECLASS/jsplus.js
+++ PRECLASS/jsplus.js
@@ -8,3 +8,26 @@ let obj = { name: "Edward", lastname: "King", age: "47" };
 for (key in obj) {
     console.log(key);
 };
+
+console.log(obj["name"]);
+let secondObj = obj.age;
+console.log(secondObj);
+
+
+
+// *****JS Objects & Classes******* //
+
+var dog = {
+    name: "Rocky",
+    legs: 4,
+    tail: 1,
+    qualities: ["loyalty", "companionship"]
+};
+
+// there are two ways to access
+
+var myDog = dog.name;
+console.log(myDog);
+
+var yourDog = dog["name"];
+console.log(yourDog);
