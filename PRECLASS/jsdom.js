131638b27ef1d7fc354317f4439265e549ad11f9
HaroldFinch129 <81425579+HaroldFinch129@users.noreply.github.com>
2021-08-10 07:55:32 +0300

    Add files via upload

--- PRECLASS/jsdom.js
+++ PRECLASS/jsdom.js
@@ -1,1 +1,24 @@
 document.getElementById("object").innerHTML = "Hello World";
+
+var x = document.getElementById("demo");
+console.log(x);
+
+var y = x.getElementsByTagName("p");
+console.log(y);
+
+
+
+
+var z = document.getElementsByTagName("p");
+console.log(z);
+
+var a = document.getElementById("DOM");
+console.log(a);
+
+a.innerHTML = "If you want, you should hardwork...";
+
+
+// x.innerHTML = "my first paragraph is ; " + y[0].innerHTML;
+
+var g = document.getElementById("first");
+console.log(g);
