e5f185ae1a8d185bb192b5c1d8f51a95853f9ab2
HaroldFinch129 <81425579+HaroldFinch129@users.noreply.github.com>
2021-08-10 22:52:32 +0300

    Add files via upload

--- PRECLASS/jsplus.js
+++ PRECLASS/jsplus.js
@@ -24,7 +24,20 @@ var dog = {
     qualities: ["loyalty", "companionship"]
 };
 
-// there are two ways to access
+// class dog {
+//     constructor(name, legs, tail) {
+//         this.name = name;
+//         this.legs = legs;
+//         this.tail = tail;
+//     }
+// }
+
+// var dog1 = new dog("Riley", 4, 1);
+// var dog2 = new dog("Ginger", 4, 1);
+
+// document.write(dog1.name); // Output: "Riley"
+// document.write(dog2.tail);
+// // there are two ways to access
 
 var myDog = dog.name;
 console.log(myDog);
@@ -52,3 +65,53 @@ var cat = {
 
 cat.meow();
 console.log(cat.result());
+
+// The Object Constructor
+
+
+
+//  After we have an object constructor, we can create 
+// **new** objects of the same type with the new keyword.
+var cat = {
+    name: "Gilbert",
+    legs: 4,
+    tail: 1,
+    qualities: ["cute", "fast", "hunter"],
+    meow: function() {
+        console.log("meowww");
+    },
+    result: function() {
+        return this.name + " has " + this.legs + " legs.";
+    }
+
+
+
+};
+
+
+
+
+
+
+class Dog {
+    constructor(name, foot) {
+        this.dogName = name;
+        this.foot = foot;
+    }
+    current() {
+        return "I have a " + this.dogName;
+    }
+}
+
+class Specy extends Dog {
+    constructor(name, foot, char) {
+        super(name, foot);
+        this.character = char;
+    }
+    show() {
+        return this.current() + ', it is  ' + this.character;
+    }
+}
+
+myDog = new Specy("Buldog", 4, "curious");
+console.log(myDog);
