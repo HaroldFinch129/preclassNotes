d4bb3f907c6c85f89f7c40ea4eb0fdf04f396a3b
HaroldFinch129 <81425579+HaroldFinch129@users.noreply.github.com>
2021-08-19 16:30:07 +0300

    Add files via upload

---
+++ jsevents.js
@@ -0,0 +1,11 @@
+function hello() {
+    alert("hello my friend");
+};
+
+function mouseOver() {
+    document.write("Mouse Over!");
+};
+
+function mouseOut() {
+    document.write("Mouse Out!");
+};
