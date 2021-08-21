e92a2070cfd896bcd5ebfae3e748789f69ca75f3
HaroldFinch129 <81425579+HaroldFinch129@users.noreply.github.com>
2021-08-21 11:27:57 +0300

    Add files via upload

--- PRECLASS/callback.js
+++ PRECLASS/callback.js
@@ -6,3 +6,12 @@ function increaseCounter(event) {
 }
 
 btnPlayer.addEventListener('click', increaseCounter);
+
+
+function square(number) {
+    return number * number
+};
+const numbers = [1, 3, 5, 7];
+const squares = numbers.map(square);
+
+console.log(squares);
