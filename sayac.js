fe7e9c8067dcce83388fd53d625147342bd18147
HaroldFinch129 <81425579+HaroldFinch129@users.noreply.github.com>
2021-08-24 22:08:23 +0300

    Add files via upload

---
+++ sayac.js
@@ -0,0 +1,37 @@
+var deger = document.getElementById("degeri-al");
+var reset = document.getElementById("reset");
+var start = document.getElementById("start");
+var stop = document.getElementById("stop");
+
+var kutu = document.getElementsByClassName("kutu")[0];
+var kutu = document.getElementsByClassName("durum")[0];
+var kutu = document.getElementsByClassName("deger")[0];
+
+reset.addEventListener("click", () => {
+            saniye.innerHTML = deger.value;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // var x = 5;
+        // setInterval(geriSay, 1000);
+
+        // function geriSay() {
+        //     if (x > 0) {
+        //         console.log(x);
+        //         x--;
+
+        //     } else {
+        //         clearInterval();
+        //     }
+        // }
