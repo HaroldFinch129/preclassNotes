c47b5f9a6999d2119011b34b9dad3a3f5bcb51d0
HaroldFinch129 <81425579+HaroldFinch129@users.noreply.github.com>
2021-08-25 16:43:21 +0300

    Add files via upload

--- sayac.js
+++ sayac.js
@@ -1,15 +1,40 @@
 var deger = document.getElementById("degeri-al");
-var reset = document.getElementById("reset");
+var programla = document.getElementById("programla");
 var start = document.getElementById("start");
 var stop = document.getElementById("stop");
 
 var kutu = document.getElementsByClassName("kutu")[0];
-var kutu = document.getElementsByClassName("durum")[0];
-var kutu = document.getElementsByClassName("deger")[0];
+var durum = document.getElementsByClassName("durum")[0];
+var saniye = document.getElementsByClassName("deger")[0];
 
-reset.addEventListener("click", () => {
-            saniye.innerHTML = deger.value;
+var x;
+
+programla.addEventListener("click", () => {
+    saniye.innerHTML = deger.value;
+    durum.classList.remove("gorun")
+
+});
+start.addEventListener("click", () => {
+    x = setInterval(geriSay, 1000);
+
+    function geriSay() {
+        var degeri_al = Number(saniye.textContent);
+        if (degeri_al > 0) {
+            degeri_al -= 1;
+            saniye.innerHTML = degeri_al.toString();
+
+
+        } else {
+            clearInterval();
+            durum.classList.add("gorun");
+
         }
+    }
+});
+
+stop.addEventListener("click", () => {
+    clearInterval(x);
+});
 
 
 
@@ -23,15 +48,15 @@ reset.addEventListener("click", () => {
 
 
 
-        // var x = 5;
-        // setInterval(geriSay, 1000);
+// var x = 5;
+// setInterval(geriSay, 1000);
 
-        // function geriSay() {
-        //     if (x > 0) {
-        //         console.log(x);
-        //         x--;
+// function geriSay() {
+//     if (x >= 0) {
+//         console.log(x);
+//         x--;
 
-        //     } else {
-        //         clearInterval();
-        //     }
-        // }
+//     } else {
+//         clearInterval();
+//     }
+// }
